5502fe68b0f506c8cd067cc847784382a18a9fcd
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 14:58:27 +0530

    basic-remote server

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * Remotely Executable
+ * Network Call
+ * Network Address
+ * http://localhost:4000/
+ */
+import express from "express";
+const app = express();
+app.listen(4000);
+
+
 function main(){
   return "Hello World";
 }
