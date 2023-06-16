1a35775ff3594f17f7b597c91fb4d8d5110a9b14
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 15:19:22 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-/**
- * 
- * Remotely Executable
- * Network Call
- * Network Address
- * http://localhost:4000/
- */
 import express from "express";
 const app = express();
-app.listen(4000);
 
-
-function main(){
-  return "Hello World";
+function main(req, res) {
+  res.send("Hello World");
 }
+function happy(req, res) {
+  res.send("I am Happy");
+}
+
+//http://localhost:4000/main
+app.get("/main", main);
+app.get("/happy", happy);
+//http://localhost:4000/main
+app.listen(4000);
