ac87609a96af25de5816202a0574ae700526ee4e
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 15:46:27 +0530

    dynamic -express=mongodb-node

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
+import { MongoClient } from "mongodb";
 import express from "express";
 const app = express();
 
-function App(req, res) {
-  res.send("Vishal");
+async function main(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+  const db = client.db("vishal");
+  const message = db.collection("message");
+
+  let inputDocument = { message: "Hello thank you GOD" };
+  await message.insertOne(inputDocument);
+  await client.close();
+  res.send("Record Added");
 }
-app.get("/App",App);
-app.get("/first", (req, res) => {
-  res.send("World");
-});
+
+async function findAllMessage(req, res) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("vishal");
+  const message = db.collection("message");
+
+  // let inputDocument = {message : "Hello GOD..Thank you so much for this life and give me chance to learn a technology"};
+  // await message.insertOne(inputDocument);
+  let list = await message.find().toArray();
+  await client.close();
+  res.send(list);
+}
+
+app.get("/main", main);
+app.get("/findAllMessage", findAllMessage);
+
 app.listen(4000);
