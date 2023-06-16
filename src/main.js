b1be30ea92f585f050370356e7cc6dc54fddbb7c
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 10:32:06 +0530

    node+MongoDB

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("Thank you GOD!!!")
+//Connect to DATABASE :: MongoDB
+import { MongoClient } from "mongodb";
+
+async function main() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+  //Selecting DATABASE
+  const db = client.db("vishal");
+  //Selecting Collection or Table from DATABASE Vishal
+  const message = db.collection("message");
+  //Ifunction nserting One Message into the database
+  /**INSERTONE is a non-blocking so We are suing await to get response from that function and make main function async  */
+  await message.insertOne({ text: "Thank You GOD" });
+
+  await client.close();
+  console.log("Record Added");
+}
+main();
+
