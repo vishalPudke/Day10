ff27a82229c33b8465e76f9b4a9e123131ca7b30
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 11:53:10 +0530

    modular program done

--- src/main.js
+++ src/main.js
@@ -1,44 +1,20 @@
 //Connect to DATABASE :: MongoDB
-import { MongoClient } from "mongodb";
 
-async function insertMessage(jsonDocument) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  const db = client.db("vishal");
-
-  const message = db.collection("message");
-
-  await message.insertOne(jsonDocument);
-
-  await client.close();
-  console.log("Record Added");
-}
-
-async function readMessage() {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  //LOGIC OF READ
-  let db = client.db("vishal");
-  let message = db.collection("message");
-
-  let list = await message.find().toArray();
-  console.log(list);
-  await client.close();
-  //return list;
-}
+import { insertMessage, readMessage } from "./mydb.js";
 
 async function main() {
+
+    //INSERT 
   let jsonDocument = {
     message: "Thank YOU GOD ",
     SentBy: "Vishal",
     to: "GOD",
     from: "Vishal",
   };
-  //await insertMesaage(jsonDocument);
-  //   let list = await readMessage();
-  //   console.log(list);
-  await readMessage();
+  await insertMessage(jsonDocument);
+  //READ.....
+  let list = await readMessage();
+  console.log(list);
+  //await readMessage();
 }
 main();
