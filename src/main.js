f44bac140902235ae7024d83e1e1ac6056bfc7c1
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 14:52:27 +0530

    expressJS -Remotely executable method

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-import { MongoClient } from "mongodb";
-
-async function main() {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  let db = client.db("vishal");
-  let message = db.collection("message");
-  let jsonDocument = { message: "Thank You God" };
-  await message.insertOne(jsonDocument);
-  await client.close();
-  console.log("Record inserted Successfully");
+function main(){
+  return "Hello World";
 }
-main();
