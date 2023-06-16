f3537f5348b38d7abab5bec624b2a9831dcf59ef
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 11:01:41 +0530

    passing -data fromargument

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 //Connect to DATABASE :: MongoDB
 import { MongoClient } from "mongodb";
 
-async function insertRecord() {
+async function insertRecord(jsonDocument) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
-  //Selecting DATABASE
+
   const db = client.db("vishal");
-  //Selecting Collection or Table from DATABASE Vishal
+
   const message = db.collection("message");
-  //Ifunction nserting One Message into the database
-  /**INSERTONE is a non-blocking so We are suing await to get response from that function and make main function async  */
-  let jsonDocument = {
-    message: "Thank YOu GOD ",
-    SentBy: "Vishal",
-    to: "GOD",
-    from: "Vishal",
-  };
+
+  //
 
   await message.insertOne(jsonDocument);
 
@@ -24,6 +18,12 @@ async function insertRecord() {
 }
 
 async function main() {
-  await insertRecord();
+  let jsonDocument = {
+    message: "Thank YOU GOD ",
+    SentBy: "Vishal",
+    to: "GOD",
+    from: "Vishal",
+  };
+  await insertRecord(jsonDocument);
 }
 main();
