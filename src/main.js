7c4315d4635beb00b6038fa645cf47a35325738c
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-16 11:07:42 +0530

    Thank you GOD:-Read message from MongoDB

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Connect to DATABASE :: MongoDB
 import { MongoClient } from "mongodb";
 
-async function insertRecord(jsonDocument) {
+async function insertMessage(jsonDocument) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
@@ -17,6 +17,19 @@ async function insertRecord(jsonDocument) {
   console.log("Record Added");
 }
 
+async function readMessage() {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  //LOGIC OF READ
+  let db = client.db("vishal");
+  let message = db.collection("message");
+
+  let list = await message.find().toArray();
+  console.log(list);
+  await client.close();
+}
+
 async function main() {
   let jsonDocument = {
     message: "Thank YOU GOD ",
@@ -24,6 +37,7 @@ async function main() {
     to: "GOD",
     from: "Vishal",
   };
-  await insertRecord(jsonDocument);
+  //await insertMesaage(jsonDocument);
+  readMessage();
 }
 main();
